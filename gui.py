2a74175ac230d2a0f4d333beabb2e582b9b9cb83
Guto Hertzog <guto.hertzog@gmail.com>
2024-10-24 11:00:01 -0300

    versão preliminar da GUI
    - primeira versão da GUI (images/screenshot_24102024_105812.jpg);
    - removida a opção de apagar o frame individualmente;
    - a ordem para adicionar/remover os frames respeitará a FILO;

--- gui.py
+++ gui.py
@@ -1,63 +1,81 @@
-from tkinter import Button, Entry, Frame, Label, Tk
-from tkinter import BOTTOM
+from tkinter import Button, Entry, Frame, Label, Menu, Tk
+from tkinter import BOTTOM, LEFT
 from tkinter.filedialog import askopenfilename
 
-fonte = ('Arial',24)
+fonte = ('Arial',16)
 
 frames = []
-indice = 0
 pdfs = []
 
-def apaga_frame(i):
-    print(f'{i = }')
-    para_apagar = frames[i]
-    del frames[i]
+def apaga_frame():
+    global frames
+    global pdfs
+    para_apagar = frames.pop()
+    pdfs.pop()
     para_apagar.destroy()
-    print(f'{len(frames) = }')
-    print(f'{pdfs = }')
+    toggle_btn()
     janela.update()
 
-def cria_frame():
-    global indice
-    frm_novo = Frame(janela)
+def limpar_pdfs():
+    global frames
+    global pdfs
+    for frame in frames:
+        frame.destroy()
+    frames = []
+    pdfs = []
+    toggle_btn()
+    janela.update()
 
+def toggle_btn():
+    global pdfs
+    if len(pdfs) > 1:
+        btn_juntar['state'] = 'normal'
+    else:
+        btn_juntar['state'] = 'disabled'
+
+def cria_frame():
     arq_path = askopenfilename(
             title='Selecione o Arquivo',
             filetypes=[("Arquivos PDF", "*.pdf")])
 
     if arq_path:
+        frm_novo = Frame(janela)
         pdfs.append(arq_path)
         arq = arq_path.split('/')[-1]
         ent_arq = Entry(frm_novo, font=fonte)
         ent_arq.insert(0, arq)
         ent_arq.grid(row=0, column=0)
-        #btn_edita = Button(frm_novo, text='E', font=fonte)
-        #btn_edita.grid(row=0, column=1)
-        btn_remove = Button(
-            frm_novo,
-            text='A',
-            font=fonte,
-            command=lambda:apaga_frame(indice))
-        indice += 1
-        btn_remove.grid(row=0, column=1)
         frm_novo.pack()
 
+        toggle_btn()
+
         frames.append(frm_novo)
-        print(f'{len(frames) = }')
-        print(f'{pdfs = }')
 
 janela = Tk()
 
 janela.title('Juntador PDFs')
 janela.minsize(300,400)
 
+menu_princ = Menu(janela)
+janela.config(menu=menu_princ)
+
+menu_sec = Menu(menu_princ, tearoff=0)
+menu_princ.add_cascade(label='Opções', menu=menu_sec)
+menu_sec.add_command(label='Sair', command=janela.quit)
+
 lbl_titulo = Label(janela, text="Bem-vindo ao Juntador de PDFs", font=fonte)
 lbl_titulo.pack()
 
-btn_teste = Button(janela, text='cria frame', command=cria_frame, font=fonte)
-btn_teste.pack()
+frm_botoes = Frame(janela)
+btn_novo_pdf = Button(frm_botoes, text='Carregar', command=cria_frame, font=fonte)
+btn_novo_pdf.pack(side=LEFT)
+btn_remove_pdf = Button(frm_botoes, text='Remover', command=apaga_frame, font=fonte)
+btn_remove_pdf.pack(side=LEFT)
+btn_limpar = Button(frm_botoes, text='Limpar', command=limpar_pdfs, font=fonte)
+btn_limpar.pack(side=LEFT)
+frm_botoes.pack()
 
-btn_juntar = Button(janela, text='Juntar', font=fonte)
+btn_juntar = Button(janela, text='Juntar', font=fonte, state='disable')
 btn_juntar.pack(side=BOTTOM)
 
 janela.mainloop()
