ccfe301191bcab3961fffdd0fe99c0cb1dadcfd0
Guto Hertzog <guto.hertzog@gmail.com>
2024-10-25 10:13:56 -0300

    ajustes readme.md e ttk
    - alterado o nome dos arquivos de setup;
    - melhorado o texto do readme, trazendo mais informações;
    - troca de alguns widgets tk para ttk;

--- README.md
+++ README.md
@@ -2,7 +2,30 @@
 
 Juntador de PDFs
 
+Repositório para o programa de juntar dois ou mais arquivos PDFs em um arquivo. Ele usa o [Python](https://www.python.org/) e a interface gráfica [tkinter](https://docs.python.org/3/library/tkinter.html).
 Repositório para guardar o programa em Tkinter que vai juntar dois ou mais arquivos PDFs.
 
-Ele usa a biblioteca [PyPDF2](https://pypi.org/project/PyPDF2/) para realizar a fusão dos arquivos e o [pyinstaller](https://pypi.org/project/pyinstaller/) para criar o executável.
+Pacotes externos :
+- [PyPDF2](https://pypi.org/project/PyPDF2/) para realizar a fusão dos arquivos;
+- [pyinstaller](https://pypi.org/project/pyinstaller/) para criar o executável;
+
+## preparação
+
+Para preparar o ambiente e realizar alterações no projeto, execute o arquivo [setup-win.bat](setup-win.bat) para sistemas Windows e [setup-unix.sh](setup-unix.sh) para Linux / macOS.
+
+Ele vai remover (caso não exista) a pasta `.venv` que é usada para configurar o ambiente virtual, criará uma nova e instalará as [dependências](requisitos.txt) para que o projeto possa ser executado diretamente do fonte.
+
+Uma vez criada a pasta do ambiente virtual, é preciso ativá-la :
+- Windows : `.venv\Scripts\activate`
+- Linux / macOS : `source .venv/bin/activate`
+
+Para executar, basta digitar no terminal / prompt de comando :
+
+```bash
+pdf-merge❯ python main.py
+```
+
+## todo
+
+- [ ] adicionar remoção individual do frame (atualmente funciona com FILO);
 
--- gui.py
+++ gui.py
@@ -1,8 +1,7 @@
-from tkinter import Button, Entry, Frame, Label, Menu, Tk
+from tkinter import Menu, Tk
+from tkinter.ttk import Button, Entry, Frame, Label
 from tkinter import BOTTOM, LEFT
 from tkinter.filedialog import askopenfilename
-
-fonte = ('Arial',16)
 
 frames = []
 pdfs = []
@@ -42,7 +41,7 @@ def cria_frame():
         frm_novo = Frame(janela)
         pdfs.append(arq_path)
         arq = arq_path.split('/')[-1]
-        ent_arq = Entry(frm_novo, font=fonte)
+        ent_arq = Entry(frm_novo)
         ent_arq.insert(0, arq)
         ent_arq.grid(row=0, column=0)
         frm_novo.pack()
@@ -63,19 +62,19 @@ menu_sec = Menu(menu_princ, tearoff=0)
 menu_princ.add_cascade(label='Opções', menu=menu_sec)
 menu_sec.add_command(label='Sair', command=janela.quit)
 
-lbl_titulo = Label(janela, text="Bem-vindo ao Juntador de PDFs", font=fonte)
+lbl_titulo = Label(janela, text="Bem-vindo ao Juntador de PDFs")
 lbl_titulo.pack()
 
 frm_botoes = Frame(janela)
-btn_novo_pdf = Button(frm_botoes, text='Carregar', command=cria_frame, font=fonte)
+btn_novo_pdf = Button(frm_botoes, text='Carregar', command=cria_frame)
 btn_novo_pdf.pack(side=LEFT)
-btn_remove_pdf = Button(frm_botoes, text='Remover', command=apaga_frame, font=fonte)
+btn_remove_pdf = Button(frm_botoes, text='Remover', command=apaga_frame)
 btn_remove_pdf.pack(side=LEFT)
-btn_limpar = Button(frm_botoes, text='Limpar', command=limpar_pdfs, font=fonte)
+btn_limpar = Button(frm_botoes, text='Limpar', command=limpar_pdfs)
 btn_limpar.pack(side=LEFT)
 frm_botoes.pack()
 
-btn_juntar = Button(janela, text='Juntar', font=fonte, state='disable')
+btn_juntar = Button(janela, text='Juntar', state='disable')
 btn_juntar.pack(side=BOTTOM)
 
 janela.mainloop()
