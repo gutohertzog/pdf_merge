fcf0174651680d45dcc76a42675db4e5c0b8caa4
Guto Hertzog <guto.hertzog@gmail.com>
2024-10-25 12:10:39 -0300

    oop e idiomas
    - convertido o projeto para POO;
    - adicionado o módulo language.py para suporte a multi-language

--- gui.py
+++ gui.py
@@ -2,80 +2,96 @@ from tkinter import Menu, Tk
 from tkinter.ttk import Button, Entry, Frame, Label
 from tkinter import BOTTOM, LEFT
 from tkinter.filedialog import askopenfilename
+from language import IDIOMAS
 
-frames = []
-pdfs = []
+class Aplicativo(Tk):
+    def __init__(self):
+        super().__init__()
+        self.idioma = IDIOMAS['pt-br']
+        self.frames = []
+        self.pdfs = []
 
-def apaga_frame():
-    global frames
-    global pdfs
-    para_apagar = frames.pop()
-    pdfs.pop()
-    para_apagar.destroy()
-    toggle_btn()
-    janela.update()
+    # métodos de criação
+    def conf_app(self):
+        """ método com as configurações da janela """
+        self.title(self.idioma['titulo-janela'])
+        self.minsize(480, 640)
 
-def limpar_pdfs():
-    global frames
-    global pdfs
-    for frame in frames:
-        frame.destroy()
-    frames = []
-    pdfs = []
-    toggle_btn()
-    janela.update()
+    def menu(self):
+        """ método com a configuração do menu superior """
+        main_menu = Menu(self)
+        self.config(menu=main_menu)
 
-def toggle_btn():
-    global pdfs
-    if len(pdfs) > 1:
-        btn_juntar['state'] = 'normal'
-    else:
-        btn_juntar['state'] = 'disabled'
+        menu_opcoes = Menu(main_menu, tearoff=0)
+        main_menu.add_cascade(menu=menu_opcoes, label=self.idioma['opcoes'])
 
-def cria_frame():
-    arq_path = askopenfilename(
-            title='Selecione o Arquivo',
-            filetypes=[("Arquivos PDF", "*.pdf")])
+        menu_opcoes.add_command(label=self.idioma['sair'], command=self.quit)
 
-    if arq_path:
-        frm_novo = Frame(janela)
-        pdfs.append(arq_path)
-        arq = arq_path.split('/')[-1]
-        ent_arq = Entry(frm_novo)
-        ent_arq.insert(0, arq)
-        ent_arq.grid(row=0, column=0)
-        frm_novo.pack()
+    def titulo(self):
+        lbl_titulo = Label(self, text=self.idioma['titulo'])
+        lbl_titulo.pack()
 
-        toggle_btn()
+    def botao_juntar(self):
+        self.btn_juntar = Button(self, text=self.idioma['merge'], state='disable')
+        self.btn_juntar.pack(side=BOTTOM)
 
-        frames.append(frm_novo)
 
-janela = Tk()
+    def frame_botoes(self):
+        frm_botoes = Frame(self)
+        btn_novo_pdf = Button(frm_botoes, text=self.idioma['load'], command=self.cria_frame)
+        btn_novo_pdf.pack(side=LEFT)
+        btn_remove_pdf = Button(frm_botoes, text=self.idioma['remove'], command=self.apaga_frame)
+        btn_remove_pdf.pack(side=LEFT)
+        btn_limpar = Button(frm_botoes, text=self.idioma['clear'], command=self.limpar_pdfs)
+        btn_limpar.pack(side=LEFT)
+        frm_botoes.pack()
 
-janela.title('Juntador PDFs')
-janela.minsize(300,400)
+    # métodos de ação
+    def apaga_frame(self):
+        para_apagar = self.frames.pop()
+        self.pdfs.pop()
+        para_apagar.destroy()
+        self.toggle_btn()
+        self.janela.update()
 
-menu_princ = Menu(janela)
-janela.config(menu=menu_princ)
+    def limpar_pdfs(self):
+        for frame in self.frames:
+            frame.destroy()
+        self.frames = []
+        self.pdfs = []
+        self.toggle_btn()
+        self.janela.update()
 
-menu_sec = Menu(menu_princ, tearoff=0)
-menu_princ.add_cascade(label='Opções', menu=menu_sec)
-menu_sec.add_command(label='Sair', command=janela.quit)
+    def toggle_btn(self):
+        if len(self.pdfs) > 1:
+            self.btn_juntar['state'] = 'normal'
+        else:
+            self.btn_juntar['state'] = 'disabled'
 
-lbl_titulo = Label(janela, text="Bem-vindo ao Juntador de PDFs")
-lbl_titulo.pack()
+    def cria_frame(self):
+        arq_path = askopenfilename(
+                title='Selecione o Arquivo',
+                filetypes=[("Arquivos PDF", "*.pdf")])
 
-frm_botoes = Frame(janela)
-btn_novo_pdf = Button(frm_botoes, text='Carregar', command=cria_frame)
-btn_novo_pdf.pack(side=LEFT)
-btn_remove_pdf = Button(frm_botoes, text='Remover', command=apaga_frame)
-btn_remove_pdf.pack(side=LEFT)
-btn_limpar = Button(frm_botoes, text='Limpar', command=limpar_pdfs)
-btn_limpar.pack(side=LEFT)
-frm_botoes.pack()
+        if arq_path:
+            frm_novo = Frame(self)
+            self.pdfs.append(arq_path)
+            arq = arq_path.split('/')[-1]
+            ent_arq = Entry(frm_novo)
+            ent_arq.insert(0, arq)
+            ent_arq.grid(row=0, column=0)
+            frm_novo.pack()
 
-btn_juntar = Button(janela, text='Juntar', state='disable')
-btn_juntar.pack(side=BOTTOM)
+            self.toggle_btn()
 
-janela.mainloop()
+            self.frames.append(frm_novo)
+
+if __name__ == '__main__':
+    app = Aplicativo()
+    app.conf_app()
+    app.menu()
+    app.titulo()
+    app.frame_botoes()
+    app.botao_juntar()
+    app.mainloop()
 
---
+++ language.py
@@ -0,0 +1,26 @@
+""" módulo para guardar as opções de idiomas """
+
+IDIOMAS = {
+    'pt-br': {
+        'titulo-janela': 'Juntador PDFs',
+        'opcoes': 'Opções',
+        'sair': 'Sair',
+        'titulo': 'Bem-vindo ao Juntador de PDFs',
+        'merge': 'Juntar',
+        'load': 'Carregar',
+        'remove': 'Remover',
+        'clear': 'Limpar',
+    },
+    'en-us': {
+        'titulo-janela': 'PDFs Merger',
+        'opcoes': 'Options',
+        'sair': 'Exit',
+        'titulo': 'Welcome to PDFs Merger',
+        'merge': 'Merge',
+        'load': 'Load',
+        'remove': 'Remove',
+        'clear': 'Clear',
+    }
+}
+
+
